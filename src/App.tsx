56becdc54a3d41ff603c480049c53f2f8adb00d9
iwgx <wahabiputra@gmail.com>
2021-03-15 11:19:27 +0700

    Make mobile friendly

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="py-8 bg-gray-100">
+      <header className="pt-8 pb-2 lg:py-8 px-8 lg:px-0 bg-gray-100">
         <div className="container mx-auto flex">
           <h1 className="text-xl">
             Smart Ordering
@@ -15,15 +15,15 @@ function App() {
         </div>
       </header>
       <main className="bg-gray-100 flex-1">
-        <div className="container mx-auto flex py-8">
-          <form className="flex flex-col bg-white px-4 py-4 rounded-md shadow-lg" onSubmit={actionSubmitMqttCredentials}>
+        <div className="container mx-auto flex flex-col lg:flex-row py-8 px-8 lg:px-0">
+          <form className="flex flex-col items-center bg-white px-4 py-4 rounded-md shadow-lg lg:w-96" onSubmit={actionSubmitMqttCredentials}>
             <h2 className="text-2xl text-center">MQTT Credential</h2>
-            <input type="text" placeholder="url" className="border rounded-md p-2 w-64 mt-4" />
-            <input type="text" placeholder="username" className="border rounded-md p-2 w-64 mt-4" />
-            <input type="password" placeholder="password" className="border rounded-md p-2 w-64 mt-4" />
-            <button className="bg-blue-500 rounded-md text-white p-2 w-64 mt-4">Submit</button>
+            <input type="text" placeholder="url" className="border rounded-md p-2 w-full mt-4" />
+            <input type="text" placeholder="username" className="border rounded-md p-2 w-full mt-4" />
+            <input type="password" placeholder="password" className="border rounded-md p-2 w-full mt-4" />
+            <button className="bg-blue-500 rounded-md text-white p-2 w-full mt-4">Submit</button>
           </form>
-          <div className="ml-8 w-full">
+          <div className="lg:ml-8 mt-8 lg:mt-0 w-full">
             <div className="px-6 py-4 bg-white rounded-lg shadow-lg">
               Silakan mengisi MQTT Credential agar bisa melihat pesanan yang masuk
             </div>
@@ -34,7 +34,7 @@ function App() {
         <div className="container mx-auto text-white py-8 px-12 text-center">
           &copy; 2021 Ilham Wahabi & Kevin Fernaldy
         </div>
-      </footer>      
+      </footer>
     </div>
   )
 }
