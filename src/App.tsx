3b8127fc77ded77ee2ec0a7d69f150f51951ee3c
iwgx <wahabiputra@gmail.com>
2021-03-15 11:00:20 +0700

    Create basic wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,40 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const actionSubmitMqttCredentials = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+    <div className="flex flex-col min-h-screen">
+      <header className="py-8 bg-gray-100">
+        <div className="container mx-auto flex">
+          <h1 className="text-xl">
+            Smart Ordering
+          </h1>
+        </div>
       </header>
+      <main className="bg-gray-100 flex-1">
+        <div className="container mx-auto flex py-8">
+          <form className="flex flex-col bg-white px-4 py-4 rounded-md shadow-lg" onSubmit={actionSubmitMqttCredentials}>
+            <h2 className="text-2xl text-center">MQTT Credential</h2>
+            <input type="text" placeholder="url" className="border rounded-md p-2 w-64 mt-4" />
+            <input type="text" placeholder="username" className="border rounded-md p-2 w-64 mt-4" />
+            <input type="password" placeholder="password" className="border rounded-md p-2 w-64 mt-4" />
+            <button className="bg-blue-500 rounded-md text-white p-2 w-64 mt-4">Submit</button>
+          </form>
+          <div className="ml-8 w-full">
+            <div className="px-6 py-4 bg-white rounded-lg shadow-lg">
+              Silakan mengisi MQTT Credential agar bisa melihat pesanan yang masuk
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="bg-gray-800">
+        <div className="container mx-auto text-white py-8 px-12 text-center">
+          &copy; 2021 Ilham Wahabi & Kevin Fernaldy
+        </div>
+      </footer>      
     </div>
   )
 }
